3fe2ad9af3ea5269bd33262c9fad6d9e6115f243
Rafael Musse <rafaelmusse@MacBook-Air-de-Rafael.local>
2022-07-26 18:53:20 -0300

    começando o projeto.

---
+++ simulator.js
@@ -0,0 +1,10 @@
+
+// em construção, apenas para marcação no perfil.
+
+let summoned;
+function roll10 {
+    for(let index = 1 ; index <= 10 ; index += 1){
+    summoned += 1
+    }
+    return summoned
+}
